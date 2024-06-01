e2c6ac3dc74946545509dd606b33fa362bae4ca8
CanvCan <golotooglusaid@gmail.com>
2024-06-02 01:58:28 +0300

    Update StructuralPatterns.Composite.Application1

--- DesignPattern/src/StructuralPatterns/Composite/Application1.java
+++ DesignPattern/src/StructuralPatterns/Composite/Application1.java
@@ -5,17 +5,17 @@ public class Application1 {
 	public static void main(String[] args) {
 		
         // Genel Müdürlük
-		Management genelMudurluk = new Management("Genel Mudurluk");
+		Management genelMudurluk = new Management("Genel Müdürlük");
 
         // Ege Bölge Müdürlüğü
-		Management egeBolgeMudurlugu = new Management("Ege Bolge Mudurlugu");
+		Management egeBolgeMudurlugu = new Management("Ege Bölge Müdürlüğü");
         egeBolgeMudurlugu.addEmployee(new Employee("Ayse", 4000));
         egeBolgeMudurlugu.addEmployee(new Employee("Can", 3000));
         egeBolgeMudurlugu.addEmployee(new Employee("Selim", 2000));
         egeBolgeMudurlugu.addEmployee(new Employee("Murat", 1500));
 
         // Akdeniz Bölge Müdürlüğü
-        Management akdenizBolgeMudurlugu = new Management("Akdeniz Bolge Mudurlugu");
+        Management akdenizBolgeMudurlugu = new Management("Akdeniz Bölge Müdürlüğü");
         akdenizBolgeMudurlugu.addEmployee(new Employee("Emre", 3000));
         akdenizBolgeMudurlugu.addEmployee(new Employee("Ahmet", 4000));
         akdenizBolgeMudurlugu.addEmployee(new Employee("Bahar", 2500));
